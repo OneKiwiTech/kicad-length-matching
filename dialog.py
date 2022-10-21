5b3bcac495150156c49c777577ae26290a5fa5ac
Van Son <thanh.duong.vs@gmail.com>
2022-10-21 15:34:02 +0700

    update dialog.py

--- dialog.py
+++ dialog.py
@@ -1,11 +1,19 @@
 import wx
+import pcbnew
 from onekiwi.controller.controller import Controller
+
+filename = '/home/vanson/working/kicad/radio-4g-imx-rt1052/iMXRT1052_Thatico.kicad_pcb'
 
 class SimplePluginApp(wx.App):
     def OnInit(self):
-        controller = Controller()
-        controller.Show()
-        return True
+        try:
+            board = pcbnew.LoadBoard(filename)
+            controller = Controller(board)
+            controller.Show()
+            return True
+        except OSError:
+            print("OSError: Unable to open file for reading.")
+            return 0
 
 def main():
     app = SimplePluginApp()
